38cf433dc3f3ff7d44c2f112cc9abc50bd23eda2
Jvymvn <Jordon.carter@yahoo.com>
2019-09-06 20:43:20 -0400

    Updated unused vars

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const dogRouter = require('./dog-router')
-const catRouter = require('./cat-router')
 const { PORT } = require('./config')
 const app = require('./app')
 
