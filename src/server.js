a4eb02b6bcf07274e45fa1387b4e46eb214ccccc
Colleen <colleenhiggins87@gmail.com>
2019-09-08 08:06:20 -0700

    v124

--- src/server.js
+++ src/server.js
@@ -20,16 +20,16 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-app.use(function errorHandler(error, req, res, next) {
+/* app.use(function errorHandler(error, req, res, next) {
   let response
-  if (NODE_ENV === 'production') {
+  /* if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } }
-  } else {
+  } else { 
     console.error(error)
     response = { message: error.message, error }
   }
   res.status(500).json(response)
-})
+}) */
 
 app.listen(PORT, () => {
   console.log('Serving on 8080');
